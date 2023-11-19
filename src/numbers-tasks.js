d291a28ec1a10429e489405622c0b81219b3c220
Clukva <berries.whale@gmail.com>
2023-11-19 03:00:02 +0300

    feat: add getFibonacciNumber function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -264,8 +264,9 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index < 2) return index;
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
